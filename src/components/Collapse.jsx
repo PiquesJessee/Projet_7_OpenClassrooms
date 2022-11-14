2fba858531e92cd168b49ed548be91cdb995755d
Piques Jessee <jesseepiques10@gmail.com>
2022-11-14 14:47:31 +0100

    Update Collapse.jsx

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState } from "react";
-// possible d'ajuster l'ouverture du collapse avec usestate
 
 const Collapse = ({ title, content }) => {
   const [isOpen, setIsOpen] = useState(true);
@@ -8,7 +7,7 @@ const Collapse = ({ title, content }) => {
     <div className='collapse'>
         <button className='collapse__button' onClick={() => setIsOpen(!isOpen)}>
           {title}
-          <p className="collapse__arrow">&lt;</p>
+          <p className={isOpen ? "collapse__arrow rotate":"collapse__arrow"}>&lt;</p>
         </button>        
         <div className={isOpen ? 'collapse__content' : 'content'}>
         {Array.isArray(content) ? (
